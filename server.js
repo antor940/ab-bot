09b9ad7c37450f401b2fb04b57a5b4a5519a846a
antor940 <78193797+antor940@users.noreply.github.com>
2021-02-24 22:18:09 +0600

    Update server.js

--- server.js
+++ server.js
@@ -20,8 +20,98 @@ client.on("ready", () => {
   client.user.setActivity("Bot created by |√|ANNTOR#8129|😎|");
 });
 
-client.on("message", msg => {
-  if (msg.content === "https://") {
-    msg.reply("@everyone https://discord.gg/3dfUfR8MZR");
+client.on('ready', () => {
+  console.log(`Logged in as ${client.user.tag}!`);
+});
+ 
+client.on('message', msg => {
+  if (msg.content === 'Hi') {
+    msg.reply('Hi');
   }
 });
+client.on('message', msg => {
+  if (msg.content === 'hi') {
+    msg.reply('hi');
+  }
+});
+client.on('message', msg => {
+  if (msg.content === 'HI') {
+    msg.reply('HI');
+  }
+});
+client.on('message', msg => {
+  if (msg.content === 'hello') {
+    msg.reply('hello');
+  }
+});
+client.on('message', msg => {
+  if (msg.content === 'Hello') {
+    msg.reply('Hello');
+  }
+});
+client.on('message', msg => {
+  if (msg.content === 'HELLO') {
+    msg.reply('HELLO');
+  }
+});
+client.on('message', msg => {
+  if (msg.content === 'bye') {
+    msg.reply('bye🙋');
+  }
+});
+client.on('message', msg => {
+  if (msg.content === 'Bye') {
+    msg.reply('Bye🙋');
+  }
+});
+client.on('message', msg => {
+  if (msg.content === 'BYE') {
+    msg.reply('BYE🙋');
+  }
+});
+client.on('message', msg => {
+  if (msg.content === 'Bye🙋') {
+    msg.reply('Bye🙋');
+  }
+});
+client.on('message', msg => {
+  if (msg.content === 'bye🙋') {
+    msg.reply('bye🙋');
+  }
+});
+client.on('message', msg => {
+  if (msg.content === 'BYE🙋') {
+    msg.reply('BYE🙋');
+  }
+});
+client.on('message', msg => {
+  if (msg.content === 'good night') {
+    msg.reply('good night😴');
+  }
+});
+client.on('message', msg => {
+  if (msg.content === 'Good night') {
+    msg.reply('Good night😴');
+  }
+});
+client.on('message', msg => {
+  if (msg.content === 'GOOD NIGHT') {
+    msg.reply('GOOD NIGHT😴');
+  }
+});
+client.on('message', msg => {
+  if (msg.content === 'GOOD MORNING') {
+    msg.reply('GOOD MORNING🥱');
+  }
+});
+client.on('message', msg => {
+  if (msg.content === 'good morning') {
+    msg.reply('good morning🥱');
+  }
+});
+client.on('message', msg => {
+  if (msg.content === 'Good morning') {
+    msg.reply('Good morning🥱');
+  }
+});
+
